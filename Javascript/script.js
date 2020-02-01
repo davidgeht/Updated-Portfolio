d0308d83ed2d8f1ca8880fee34642110e0283250
David Gehtman <davidgehtman.91@gmail.com>
2020-02-01 09:53:09 -0500

    added a more projects

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,10 +5,9 @@ class ProjectDescription {
         this.gitHub = gitHub;
         this.link = link;
         this.appendModal = function () {
-            $('.modal-title').empty();
-            $(`.modal-body`).empty();
-            $('.modal-title').append(`<h2>${this.title}</h2>`);
-            $(`.modal-body`).append(`<h4>Description:</h4> ${this.description}`, `<h4>Githud Repository:</h4><a href="${this.gitHub}">${this.gitHub}</a>`, `<h4>Link:</h4> <a href="${this.link}">${this.link}</a>`);
+            console.log("appending" + title)
+            $("#modal .modal-title ").html(`<h2>${this.title}</h2>`);
+            $("#modal .modal-body").html(`<h4>Description:</h4> ${this.description}<br><h4>Githud Repository:</h4><a href="${this.gitHub}">${this.gitHub}</a><br><h4>Link:</h4> <a href="${this.link}">${this.link}</a>`);
         };
     }
 }
@@ -26,8 +25,51 @@ const WeatherDas = new ProjectDescription(
     `https://github.com/davidgeht/Weather-Applicatio`,
     `https://davidgeht.github.io/Weather-Application/`);
 
-$("#PBmodal").click(PartyBuddy.appendModal());
-$("#WDmodal").click(WeatherDas.appendModal());
+const employeeSumApp = new ProjectDescription(
+    'Employee Summary Application',
+    `A software engineering team generator command line applicationThe code quiz application is a refresher quiz for javascript based questions, it is meant to test the user on there knoweldge of javascript. It is a mutliple choice based quiz that has a timer limit. For every question that is incorrect the user loses 15 seconds from the timer, if the timer expired to 0 the game is ended and there score is recorded in local storage which can be view on the next page titled highscores.`,
+    'https://github.com/davidgeht/Employee-Summary-Application',
+    `Not Applicable`
+);
 
+const EatDaBurger= new ProjectDescription(
+    'Eat-The-Burger',
+    'A resturant application that allows users to input the names of the burgers they would like to eat. The application allows the user to add burgers, and list the burgers in a devoured section indication the burger has been eaten.',
+    'https://github.com/davidgeht/Eat-The-Burger',
+    'https://polar-dawn-91505.herokuapp.com/'
 
+);
+
+const employeeTracker = new ProjectDescription(
+    `Employee Tracker`,
+    `An application that helps track and manage a companys' or instituations employees.`,
+    `https://github.com/davidgeht/Employee-Tracker`,
+    `Not Applicable`
+);
+
+const buckeatlist = new ProjectDescription(
+    `BuckEATList`,
+    `An application that allows the user to search and save different resturants that they want to try to their buckEATlist. Also the user is able to review and rate the resturants that they have tried in their "NOMs" list. The user is able to upload pictures from their resturant experience.`,
+    `https://github.com/davidgeht/BuckEATlist`,
+    `https://buckeatlist.herokuapp.com/`
+);
+
+$("#PBmodal").click(()=>PartyBuddy.appendModal());
+$("#WDmodal").click(()=>WeatherDas.appendModal());
+$("#CDmodal").click(()=>employeeSumApp.appendModal());
+$("#DPmodal").click(()=>EatDaBurger.appendModal());
+$("#ETmodal").click(()=>employeeTracker.appendModal());
+$("#BELmodal").click(()=>buckeatlist.appendModal());
+
+// $( document ).ready(function(){
+//     $('img').each(function(e){
+//       var src = $(e).attr('src');
+//       $(e).hover(function(){
+//         $(this).attr('src', src.replace('.gif', '_anim.gif'));
+//       }, function(){
+//         $(this).attr('src', src);
+//       });
+//     });
+//   });
+  
  
